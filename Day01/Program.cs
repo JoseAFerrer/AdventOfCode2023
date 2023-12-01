aa7677154936149daa1729e5d56ffc18b4c725d9
JoseAFerrer <seriamente.fs@gmail.com>
2023-12-02 00:18:49 +0100

    Get to complex solution

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,8 +1,23 @@
 ﻿var path = "input.txt";
 
 var readText = File.ReadAllLines(path);
+var aggregatedText = string.Join(",", readText);
+
+var refinedText = aggregatedText
+                            .Replace("one", "one1one")
+                            .Replace("two", "two2two")
+                            .Replace("three", "three3three")
+                            .Replace("four", "four4four")
+                            .Replace("five", "five5five")
+                            .Replace("six", "six6six")
+                            .Replace("seven", "seven7seven")
+                            .Replace("eight", "eight8eight")
+                            .Replace("nine", "nine9nine");
+
+var refinedLines = refinedText.Split(",");
+
 var totalSum = 0;
-foreach (var s in readText)
+foreach (var s in refinedLines)
 {
     var onlyNumbers = s.Where(char.IsNumber).Select(x => x.ToString());
     var firstLast = onlyNumbers.First() + onlyNumbers.Last();
