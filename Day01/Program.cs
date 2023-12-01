fcc4df6da93fb237a771f1eeedec0af0dd0fd073
JoseAFerrer <seriamente.fs@gmail.com>
2023-12-01 23:27:25 +0100

    Initial commit

---
+++ Day01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var path = "input.txt";
+
+var readText = File.ReadAllLines(path);
+foreach (var s in readText)
+{
+    Console.WriteLine(s);
+}
+
+Console.WriteLine("Hello, World!");
